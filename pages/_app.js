e9fc0c40313d3d493d3465e82179ffc60f499e96
jumelfuentesacrm-ui <jumelfuentesacrm@gmail.com>
2026-05-14 01:51:35 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="icon" href="/favicon.png" type="image/png"/>
         <link rel="manifest" href="/manifest.json"/>
-        <meta name="theme-color" content="#0e0e0c"/>
+        <meta name="mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
         <meta name="apple-mobile-web-app-title" content="A+ CRM"/>
